1e4f77aeecdabb839271664befccf39d65d8ebdc
Qaliss <163942600+Qaliss@users.noreply.github.com>
2025-04-05 20:04:52 -0500

    Added other files to the package

---
+++ launch/trashbot_launch.py
@@ -0,0 +1,14 @@
+import os
+import launch
+from launch import LaunchDescription
+import launch_ros.actions
+
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': 'package://trashbot/urdf/garbagebot.urdf'}],
+        )
+    ]
